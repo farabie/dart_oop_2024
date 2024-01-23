f06d5f4800e1fd25728215a286ab763a811aa9a3
Muhammad Farabie <farabie024@gmail.com>
2024-01-23 07:17:13 +0700

    dart_oop_2024

---
+++ bin/1.Class/perkenalan_class.dart
@@ -0,0 +1,10 @@
+void main() {
+  /**
+   * Class adalah blueprint, prototype atau cetakan untuk membuat sebuah objek 
+   * Class berisikan semua deklrasi semua properties dan function yang dimiliki objek
+   * Setiap Objek selalu dibuat oleh class 
+   * Dan Sebuah Class bisa membuat objek tanpa batas
+   */
+}
+
+class Person {}
---
+++ bin/dart_oop_2024.dart
@@ -0,0 +1,5 @@
+import 'package:dart_oop_2024/dart_oop_2024.dart' as dart_oop_2024;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_oop_2024.calculate()}!');
+}
